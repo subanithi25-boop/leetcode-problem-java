4585711a1c3d49e4706943e6bff420a9d89d0f36
Subanithi R <subanithi25@gmail.com>
2026-02-08 11:44:11 +0530

    Time: 118 ms (5.51%) | Memory: 48 MB (58.40%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int n = nums.length;
+        int res[] = new int[2];
+        boolean[] b = new boolean[n];
+        int k = 0;
+        for(int i = 0; i < n; i++){
+            if(b[i])
+            continue;
+            int c = 0;
+            for(int j = 0; j < n; j++){
+                if(nums[i] == nums[j]){
+                    c++;
+                    b[j] = true;
+                }
+            }
+            if(c == 1){
+                res[k++] = nums[i];
+            }
+        }
+        return res;
+    }
+}
