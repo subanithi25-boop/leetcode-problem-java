bb33743833a89befa063a99a2fe569f863dd5524
Subanithi R <subanithi25@gmail.com>
2026-02-13 15:40:43 +0530

    Time: 1 ms (59.97%) | Memory: 42.3 MB (92.89%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -1,12 +1,11 @@
-class Solution {
+public class Solution {
     public int reverseBits(int n) {
-        StringBuilder sb = new StringBuilder();
-        for(int i=0 ; i<32 ; i++){
-            int r=n%2;
-            if(r<0) r+=2;
-            sb.append(r);
-            n/=2;
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;          
+            result |= (n & 1);     
+            n >>= 1;               
         }
-        return (int) Long.parseLong(sb.toString(),2);
+        return result;
     }
 }
