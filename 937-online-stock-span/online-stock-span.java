07b30a3b18e75970950f47b8386318515c5fb7c0
Subanithi R <subanithi25@gmail.com>
2026-03-17 14:31:13 +0530

    Time: 31 ms (55.52%) | Memory: 54.4 MB (71.26%) - LeetSync

--- 937-online-stock-span/online-stock-span.java
+++ 937-online-stock-span/online-stock-span.java
@@ -7,7 +7,8 @@ class StockSpanner {
     public int next(int price) {
         int span = 1;
         while(!st.isEmpty() && price >= st.peek()[0]){
-            span += st.pop()[1];
+            span += st.peek()[1];
+            st.pop();
         }
         st.push(new int[]{price, span});
         return span;
