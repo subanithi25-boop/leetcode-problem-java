27daacad07f127eec8125c6cf73a74953072681e
Subanithi R <subanithi25@gmail.com>
2026-05-28 15:57:55 +0530

    Time: 123 ms (58.59%) | Memory: 48.2 MB (30.30%) - LeetSync

---
+++ 4084-maximize-sum-of-squares-of-digits/maximize-sum-of-squares-of-digits.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String maxSumOfSquares(int num, int sum) {
+        if(sum > 9 * num){
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < num; i++){
+            int d = Math.min(9, sum);
+            sb.append(d);
+            sum -= d;
+        }
+        return sb.toString();
+    }
+}
