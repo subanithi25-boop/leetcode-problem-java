3d3e509d7b3bd7772ee37a5c9b829af45bb1e56d
Subanithi R <subanithi25@gmail.com>
2026-05-31 19:40:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (6.20%) - LeetSync

---
+++ 520-detect-capital/detect-capital.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if(word.length() == 1){
+            return true;
+        } 
+        if(word.charAt(0) >= 'A' && word.charAt(0) <= 'Z'){
+            if(word.charAt(1) >= 'A' && word.charAt(1) <= 'Z'){
+                for(int i = 2; i < word.length(); i++){
+                    if(word.charAt(i) >= 'A' && word.charAt(i) <= 'Z'){
+                        continue;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+            }
+            else{
+                for(int i = 2; i < word.length(); i++){
+                    if(word.charAt(i) >= 'a' && word.charAt(i) <= 'z'){
+                        continue;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+            }
+        }
+        else{
+            for(int i = 1; i < word.length(); i++){
+                if(word.charAt(i) >= 'a' && word.charAt(i) <= 'z'){
+                        continue;
+                    }
+                    else{
+                        return false;
+                    }
+            }
+        }
+        return true;
+    }
+}
