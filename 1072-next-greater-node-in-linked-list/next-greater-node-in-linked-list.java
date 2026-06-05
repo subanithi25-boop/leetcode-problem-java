03914a662e12d2d8b740211441da80e1c1529244
Subanithi R <subanithi25@gmail.com>
2026-06-05 18:14:18 +0530

    Time: 25 ms (51.65%) | Memory: 50.2 MB (27.66%) - LeetSync

--- 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.java
+++ 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.java
@@ -10,20 +10,25 @@
  */
 class Solution {
     public int[] nextLargerNodes(ListNode head) {
-        List<Integer> list = new ArrayList<>();
-        while (head != null) {
+        ArrayList<Integer> list = new ArrayList<>();
+        while(head != null){
             list.add(head.val);
             head = head.next;
         }
+        Stack<Integer> st = new Stack<>();
         int n = list.size();
         int[] ans = new int[n];
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (list.get(j) > list.get(i)) {
-                    ans[i] = list.get(j);
-                    break;
-                }
+        for(int i = n - 1; i >= 0; i--){
+            while(!st.isEmpty() && st.peek() <= list.get(i)){
+                st.pop();
             }
+            if(st.isEmpty()){
+                ans[i] = 0;
+            }
+            else{
+                ans[i] = st.peek();
+            }
+            st.push(list.get(i));
         }
         return ans;
     }
